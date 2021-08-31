20b833354dcb3fc0eeb790d184b13d9c89ced177
Hector Garcia <hector.garcia07@utrgv.edu>
2021-08-30 19:25:16 -0500

    Finished mobile view and tip calculations

--- public/js/script.js
+++ public/js/script.js
@@ -4,74 +4,100 @@ const billErrorMsg = document.getElementById("bill-error-msg")
 const peopleErrorMsg = document.getElementById("people-error-msg")
 const numPeople = document.getElementById("num-people")
 const billInput = document.getElementById("bill")
-const tipPerPerson = document.getElementById("tip-per-person")
-const totalPerPerson = document.getElementById("total-per-person")
+const tipPerPersonNode = document.getElementById("tip-per-person")
+const totalPerPersonNode = document.getElementById("total-per-person")
 const customPercent = document.getElementById("custom-percent")
 const tipErrorPercent = document.getElementById("tip-error-msg")
 
-let hasError = false
+const BILL_REGEX = /^\d+$|^\d+(?:\.\d{2})$/
+const NUM_REGEX = /^[1-9]\d*$/
+const BILL_ERR_MSG = "Must be in USD format."
+const NUM_ERR_MSG = "Must be positive"
 
 //Is used to check if the user input is correct, then enables the submit btn.
 tipForm.addEventListener('input', event => {
-    let newBillInput = billInput.value 
-    let newNumPeople = numPeople.value
     let targetName = event.target.name
-    let percentAmout;
+    let percentAmount;
 
     //enable button to reset at anytime
     resetBtn.disabled = false;
 
     //try getting the current selected tip-amount. set it to null if error occurs
     try {
-        percentAmout = document.querySelector('input[name="tip-amount"]:checked').id
+        percentAmount = document.querySelector('input[name="tip-amount"]:checked').id
     } catch (error) {
-        percentAmout = null;
+        percentAmount = customPercent.value;
     }
 
     //if there is errors, display approrate message
-    //otherwise check if form has been filled out, if so, do the calculations
-    hasError = checkForErrors(targetName, event.target.type) || false
+    checkForErrors(targetName, event.target.type)
 
-    if(hasError){
-        console.log("error")
+    //check to see if all inputs are valid, if not, don't do calculations
+    if(isAllValid()){
+        calculateTip(billInput.value, percentAmount, numPeople.value)
     }
     
-
-
 })
 
-//if custom percent is clicked, check if any percent radio btn has been clicked
-//if it is, uncheck it.
-customPercent.addEventListener("input", event => {
-    let percentAmout;
+//calculates tip with the help of currency.js
+//Source: https://github.com/scurker/currency.js/
+function calculateTip(bill, tip, numPeople){
+    const totaltip = currency(Number(bill)).multiply((Number(tip) / 100))
+    const tipPerPerson = totaltip.divide(Number(numPeople))
+    const totalPerPerson = currency(Number(bill)).add(totaltip).divide(Number(numPeople))
+
+    //displays the answer to the user
+    totalPerPersonNode.innerText = totalPerPerson.format()
+    tipPerPersonNode.innerText = tipPerPerson.format()
+}
+
+
+//Will return true if all user input is valid
+function isAllValid(){
+    return (
+        BILL_REGEX.test(billInput.value) && 
+        (NUM_REGEX.test(customPercent.value) || 
+        document.querySelector('input[name="tip-amount"]:checked')) &&  
+        NUM_REGEX.test(numPeople.value)
+        )
+}
+
+//if custom percent is clicked, check if any other percent radio btn has been clicked
+//if it is, uncheck it. then calculate input if all input is valid
+customPercent.addEventListener("click", event => {
+    let percentAmount;
+
     //try getting the current selected tip-amount. set it to null if error occurs
     try {
-        percentAmout = document.querySelector('input[type="radio"][name="tip-amount"]:checked')
-        percentAmout.checked = false
+        percentAmount = document.querySelector('input[type="radio"][name="tip-amount"]:checked')
+        percentAmount.checked = false
+        
+        //check to see if all inputs are valid, if not, don't do calculations
+        if(isAllValid()){
+            calculateTip(billInput.value, customPercent.value, numPeople.value)
+        }
     } catch (error) {
         percentAmout = null;
     }
 
+    
 })
 
-//checks to see if the input accepts the users value
-//if not display error
+//Check if user's input is correct based on the name attribute
+//if incorrect display error
 function checkForErrors(inputName, inputType){
-    switch(inputName){
-        case "bill":
-            return isInputValid(billInput, /^\d+$|^\d+(?:\.\d{2})$/, "Must be in USD format.", billErrorMsg)
-            break;
-        case "tip-amount":
-            if(inputType == "text"){
-                return isInputValid(customPercent, /^\d+$/, "Must be positive", tipErrorPercent)
-            }else{
-                tipErrorPercent.innerText = ""
-            }
-            return true
-            break
-        case "num-people":
-            return isInputValid(numPeople, /^\d+$/, "Must be positive", peopleErrorMsg)
-            break
+    if(inputName == "bill"){
+        isInputValid(billInput, BILL_REGEX, BILL_ERR_MSG, billErrorMsg)
+    }
+    else if(inputName == "tip-amount"){
+        if(inputType == "text"){
+            isInputValid(customPercent, NUM_REGEX, NUM_ERR_MSG, tipErrorPercent)
+        }else{
+            tipErrorPercent.innerText = ""
+        }
+    }
+    else if(inputName == "num-people"){
+        isInputValid(numPeople, NUM_REGEX, NUM_ERR_MSG, peopleErrorMsg)
     }
 }
 
@@ -82,22 +108,17 @@ if invalid, show approprate error message
 function isInputValid(domNode, regex, errMessage, domErrorDiv){
     let isValid = regex.test(domNode.value)
 
-    if(!isValid){
-        domErrorDiv.innerText = errMessage
-        domNode.classList.add("error-border")
-    }else{
-        domErrorDiv.innerText = ""
-        domNode.classList.remove("error-border")
-    }
-
-    return isValid
+    domErrorDiv.innerText = !isValid ? errMessage : ""
 }
 
 //on reset, it resets the form and it's error message
+//resets input value to 0
 resetBtn.addEventListener('click', event => {
     billErrorMsg.innerText = ""
     peopleErrorMsg.innerText = ""
     tipErrorPercent.innerText = ""
+    totalPerPersonNode.innerText = "$0.00"
+    tipPerPersonNode.innerText = "$0.00"
 
     resetBtn.disabled = true
 })
--- src/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'))
-
-app.listen(3000, () => console.log("Listeing on port ", 3000))
